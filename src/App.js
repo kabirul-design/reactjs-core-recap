d1b52309b91c0caeaff5739c10f3524a38cd1d6c
kabirulpHero <kabirulislamuo@gmail.com>
2022-03-22 22:56:19 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Posts></Posts>
+      <h1>hello recap jsX</h1>
+      <District name="Dhaka" specialty="maramari"></District>
+      <District name="Jhenaidah" specialty="vodrota" ></District>
+      <District name="khulna" specialty="quality" ></District>
     </div>
   );
+}
+function Posts(){
+  const [posts, setPosts] = useState([]);
+
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  },[])
+
+  return(
+    <div>
+      <h1>Posts {posts.length}</h1>
+      {
+        posts.map(post => <Post name={post.title}></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props){
+  return(
+    <div>
+      <h2>{props.title}</h2>
+    </div>
+  )
+}
+
+const districtStyle ={
+  backgroundColor : 'crimson',
+  padding : '20px',
+  margin : '20px'
+
+}
+
+
+
+function District(props){
+  const  [power, setPower] = useState(1);
+  const boostPower = () =>{
+    const powerBoost = power * 2;
+    setPower(powerBoost);
+  }
+  return(
+    <div style={districtStyle}>
+      <h2>Name : {props.name} </h2>
+      <h3>Specialty : {props.specialty}</h3>
+      <p>Power : {power}</p>
+      <button onClick={boostPower}>boost the power</button>
+    </div>
+  )
 }
 
 export default App;
